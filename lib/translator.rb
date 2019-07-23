b7666ce643865d608fd1668d8a35b2adf41525d8
Jared Burnham <zlolris97@gmail.com>
2019-07-23 02:47:04 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -26,7 +26,7 @@ def get_japanese_emoticon(file_path, emoticon)
    if e.length > 2
      return e
    else
-     binding.pry
+     #binding.pry
      puts "Sorry, that emoticon was not found"
    end
 end
