91098d62bfd5d20d09168bdc910a2538d0787c68
Jared Burnham <zlolris97@gmail.com>
2019-07-23 02:45:21 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,10 +15,11 @@ end
 
 def get_japanese_emoticon(file_path, emoticon)
   load_library(file_path)
+  e = ""
   load_library(file_path).each do |k, emote|
       emote.each do |v|
         if v[0] == emoticon
-           v[1] = e
+           e = v[1]
         end
       end
     end
