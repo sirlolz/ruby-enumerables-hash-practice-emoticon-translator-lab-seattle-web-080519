b8b96b62635052ccb5bc37c76bf07840e24cead5
Jared Burnham <zlolris97@gmail.com>
2019-07-23 02:55:32 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -28,12 +28,12 @@ end
 
 def get_english_meaning(file, emoticon)
   load_library(file)
-  #load_library(file).each do |k, emote|
+  load_library(file).each do |k, emote|
    # if k == "get_meaning"
     #  emote.each do |emotion|
      #    return emotion[1]
      #   end
     #  end
   #  end
-#  end
+  end
 end
