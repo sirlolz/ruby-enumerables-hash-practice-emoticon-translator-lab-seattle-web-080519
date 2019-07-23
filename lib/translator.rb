4df4effc21e338b942dc788899209405a0ef71f7
Jared Burnham <zlolris97@gmail.com>
2019-07-23 02:42:14 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -23,7 +23,7 @@ def get_japanese_emoticon(file_path, emoticon)
         end
       end
     end
-  return puts " Sorry, that emoticon was not found"  
+   puts " Sorry, that emoticon was not found"  
 end
 
 def get_english_meaning
